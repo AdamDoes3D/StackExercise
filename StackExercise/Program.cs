673a2ecaeda5b1799ac42d389bc33db98e131af2
Adam Rhodes <jrhodes1183@gmail.com>
2019-01-27 15:59:55 -0600

    Stack Implementation

---
+++ StackExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StackExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            //stack.Push(null);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack.Pop());
+
+            //stack.Clear();
+            //stack.Clear();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StackExercise/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackExercise
+{
+    class Stack
+    {
+        private readonly List<object> _stackOfObjects;
+
+        public Stack()
+        {
+            _stackOfObjects = new List<object>();
+            //Console.WriteLine("New stack has been created.");
+        }
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new NullReferenceException();
+            }
+            _stackOfObjects.Add(obj);
+            //Console.WriteLine($"{obj} has been added to the stack.");
+        }
+
+        public object Pop()
+        {
+            int numberOfItems = _stackOfObjects.Count;
+
+            if (numberOfItems == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            var popThisObject = _stackOfObjects[numberOfItems - 1];
+            _stackOfObjects.RemoveAt(numberOfItems - 1);
+            return popThisObject;
+        }
+
+        public void Clear()
+        {
+            if (_stackOfObjects.Count == 0)
+            {
+                Console.WriteLine("Cannot clear an empty stack.");
+            }
+            else
+            {
+                _stackOfObjects.Clear();
+                //Console.WriteLine("Stack has been cleared");
+            }
+        }
+    }
+}
